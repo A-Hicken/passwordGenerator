8da9693f1b06a69152059a11c29b7b09496737aa
A-Hicken <amberlie.hicken@gmail.com>
2023-11-17 16:18:28 -0700

    script work

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-// length value of first input
-let length = parseInt(document.querySelector("length").value);
-//sets a random index
-let randomIndex = Math.floor(Math.random() * charSet.length);
+// DOM elements
+const uppercaseEl = document.getElementById("uppercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  Symbol: getRandomSymbol,
+};
+
+generateEl.addEventListener("click", () => {
+  const length = +lengthEl.value;
+
+  console.log(typeof length);
+});
+// generator functions - http://www.net-comber.com/charset.html
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+}
+
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*()_+{};~?,./";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+//console.log(getRandomSymbol());
